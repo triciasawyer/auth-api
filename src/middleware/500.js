ceeb09525df74f8d25ad29501c6165ed9a24c156
Tricia Sawyer <tricia.sawyer03@gmail>
2023-05-24 18:07:39 -0500

    scaffolding

---
+++ src/middleware/500.js
@@ -0,0 +1,15 @@
+'use strict';
+
+
+module.exports = (error, req, res, next) => {
+  // ternary wtf
+  const errorMessage = typeof(error) === 'string' ? error : error.message;
+  res.status(500).send({
+    error: 500,
+    route: req.path,
+    query: req.query,
+    path: req.params,
+    body: req.body,
+    message: `Server Error: ${errorMessage}`,
+  })
+};
